d7a83a87c304ad80b6db9f72478eddc608469ea2
Anita Kucieba <akucieba@gmail.com>
2018-02-24 15:28:09 +0100

    multiple values with letters

--- src/test/java/com/sda/cucumber/CalcServiceSteps.java
+++ src/test/java/com/sda/cucumber/CalcServiceSteps.java
@@ -33,6 +33,11 @@ public class CalcServiceSteps {
         text = "";
     }
 
+    @And("^I pass text with letters$")
+    public void I_pass_text_with_letters(){
+        text = "3a;2;4;5b";
+    }
+
     @When("^I execute calculate method$")
     public void I_execute_calculate_method() {
         actual = calcService.calculate(text);
@@ -46,5 +51,10 @@ public class CalcServiceSteps {
     @Then("^I get 0 as a result$")
     public void I_get_0_as_a_result() {
         Assert.assertEquals(0, actual);
+    }
+
+    @Then("^I get result for text with letters$")
+    public void I_get_result_for_text_with_letters(){
+        Assert.assertEquals(14, actual);
     }
 }
