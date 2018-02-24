e4bdbaadb62311f6388c48f357b9216b77832020
Anita Kucieba <akucieba@gmail.com>
2018-02-24 15:57:11 +0100

    refactor to parametrized steps

--- src/test/java/com/sda/cucumber/CalcServiceSteps.java
+++ src/test/java/com/sda/cucumber/CalcServiceSteps.java
@@ -18,29 +18,19 @@ public class CalcServiceSteps {
         calcService = new CalcService();
     }
 
-    @And("^I pass text value to compute$")
-    public void I_pass_text_value_to_compute() {
-        text = "2;3;4";
+    @And("^I pass (.*) value to compute$")
+    public void I_pass_text_value_to_compute(String text) {
+        this.text = text;
     }
 
-    @And("^I pass null text value$")
-    public void I_pass_null_text_value() {
+    @And("^I pass null$")
+    public void I_pass_null() {
         text = null;
     }
 
-    @And("^I pass blank text value$")
-    public void I_pass_blank_text_value() {
+    @And("^I pass blank$")
+    public void I_pass_blank() {
         text = "";
-    }
-
-    @And("^I pass text with letters$")
-    public void I_pass_text_with_letters(){
-        text = "3a;2;4;5b";
-    }
-
-    @And("^I pass recipe$")
-    public void I_pass_recipe(){
-        text = "2 apples;3 eggs;5butters";
     }
 
     @When("^I execute calculate method$")
@@ -48,23 +38,9 @@ public class CalcServiceSteps {
         actual = calcService.calculate(text);
     }
 
-    @Then("^I get correct result$")
-    public void I_get_correct_result() {
-        Assert.assertEquals(9, actual);
+    @Then("^I get (.*) as a result$")
+    public void I_get_value_as_a_result(int result) {
+        Assert.assertEquals(result, actual);
     }
 
-    @Then("^I get 0 as a result$")
-    public void I_get_0_as_a_result() {
-        Assert.assertEquals(0, actual);
-    }
-
-    @Then("^I get result for text with letters$")
-    public void I_get_result_for_text_with_letters(){
-        Assert.assertEquals(14, actual);
-    }
-
-    @Then("^I get result for recipe$")
-    public void I_get_result_for_recipe(){
-        Assert.assertEquals(10,actual);
-    }
 }
